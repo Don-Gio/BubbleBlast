db68ae0c24037b5887d1d83140689e6ccef6ea7e
Don-Gio <NAN>
2020-12-13 00:29:35 +0100

    Inizio

---
+++ funzioni.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "funzioni.h"
+
+//corpo delle funzioni che useró nell'altro programma
---
+++ funzioni.h
@@ -0,0 +1,6 @@
+#ifndef FUNZIONI_H
+#define FUNZIONI_H
+
+//Prototipy delle funzioni usate nel programma
+
+#endif
